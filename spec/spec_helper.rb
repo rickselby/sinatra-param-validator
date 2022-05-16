643bb4646d944044998ce80fb062f2070c6747a7
Rick Selby <rick@selby-family.co.uk>
2022-05-16 15:05:54 +0100

    Rename to sinatra-param-validator

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'sinatra/params_validator'
+require 'sinatra/param_validator'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
