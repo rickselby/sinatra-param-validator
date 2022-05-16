fccd1a31459192479093561336ad67ea979b7ab7
Rick Selby <rick@selby-family.co.uk>
2022-05-16 22:48:17 +0100

    Start work on string validation

---
+++ lib/sinatra/param_validator/parameter/string.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+module Sinatra
+  class ParamValidator
+    class Parameter
+      # Validation for strings
+      class String
+        attr_reader :errors, :value
+
+        def initialize(value, *args)
+          @args = args
+          @errors = {}
+          @value = value
+        end
+
+        def coerce
+          String(@value)
+        end
+
+        def validate
+          @args.each do |key, value|
+            raise "Unknown option '#{key}' for #{self.class}" unless respond_to? key, true
+
+            method(key).call(value)
+          end
+        end
+
+        private
+
+        def blank(enabled)
+          @errors.push 'Parameter cannot be blank' if !enabled && @value == ''
+        end
+
+        def format(format_string)
+          @errors.push 'Parameter cannot be blank' unless @value&.match?(format_string)
+        end
+
+        def is(option_value)
+          @errors.push "Parameter must be #{option_value}" unless @value == option_value
+        end
+
+        def in(options)
+          @errors.push "Parameter must be within #{options}" unless in? options
+        end
+
+        def in?(options)
+          case options
+          when Range
+            options.include? @value
+          else
+            Array(options).include? @value
+          end
+        end
+
+        def required(enabled)
+          @errors.push 'Parameter is required' if enabled && @value.nil?
+        end
+      end
+    end
+  end
+end
