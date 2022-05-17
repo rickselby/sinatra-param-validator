e4f4cd1ffa4c178759d9017d722dafa0ca9063d1
Rick Selby <richard.selby@york.ac.uk>
2022-05-17 08:18:42 +0100

    Add spec tests for integer validation

---
+++ lib/sinatra/param_validator/parameter/common.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+module Sinatra
+  class ParamValidator
+    class Parameter
+      # Validation for integers
+      module Common
+        attr_reader :errors, :value
+
+        def initialize(value, **kwargs)
+          @errors = []
+          @value = value
+
+          validate(kwargs)
+        end
+
+        def valid?
+          @errors.empty?
+        end
+
+        def validate(options)
+          options.each do |key, value|
+            raise "Unknown option '#{key}' for #{self.class}" unless respond_to? key, true
+
+            method(key).call(value)
+          end
+        end
+
+        def in(options)
+          @errors.push "Parameter must be within #{options}" unless in? options
+        end
+        private :in
+
+        def in?(options)
+          case options
+          when Range
+            options.include? @value
+          else
+            Array(options).include? @value
+          end
+        end
+        private :in?
+
+        def is(option_value)
+          @errors.push "Parameter must be #{option_value}" unless @value == option_value
+        end
+        private :is
+
+        def required(enabled)
+          @errors.push 'Parameter is required' if enabled && @value.nil?
+        end
+        private :required
+      end
+    end
+  end
+end
--- lib/sinatra/param_validator/parameter/string.rb
+++ lib/sinatra/param_validator/parameter/string.rb
@@ -1,35 +1,18 @@
 # frozen_string_literal: true
 
+require_relative 'common'
+
 module Sinatra
   class ParamValidator
     class Parameter
       # Validation for strings
       class String
-        attr_reader :errors, :value
-
-        def initialize(value, **kwargs)
-          @errors = []
-          @value = value
-
-          validate(kwargs)
-        end
+        include Common
 
         def coerce
           return nil if @value.nil?
 
           String(@value)
-        end
-
-        def valid?
-          @errors.empty?
-        end
-
-        def validate(options)
-          options.each do |key, value|
-            raise "Unknown option '#{key}' for #{self.class}" unless respond_to? key, true
-
-            method(key).call(value)
-          end
         end
 
         private
@@ -40,27 +23,6 @@ module Sinatra
 
         def format(format_string)
           @errors.push "Parameter must match the format #{format_string}" unless @value&.match?(format_string)
-        end
-
-        def is(option_value)
-          @errors.push "Parameter must be #{option_value}" unless @value == option_value
-        end
-
-        def in(options)
-          @errors.push "Parameter must be within #{options}" unless in? options
-        end
-
-        def in?(options)
-          case options
-          when Range
-            options.include? @value
-          else
-            Array(options).include? @value
-          end
-        end
-
-        def required(enabled)
-          @errors.push 'Parameter is required' if enabled && @value.nil?
         end
       end
     end
