68c8b2b99b72c7a4d2ef9069df58b0b2cc652883
Rick Selby <richard.selby@york.ac.uk>
2022-05-17 21:09:24 +0100

    Add spec test for condition syntax

---
+++ spec/sinatra/param_validator_condition_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'sinatra/test_helpers'
+
+describe Sinatra::ParamValidator do
+  include Sinatra::TestHelpers
+  before do
+    klass = described_class
+    mock_app do
+      register klass
+
+      validator :identifier do
+        param :number, Integer, required: true
+      end
+
+      post '/', validate: :identifier do
+        'OK'.to_json
+      end
+    end
+  end
+
+  it 'returns OK for valid numbers' do
+    post '/', { number: '10'}
+    expect(last_response).to be_ok
+  end
+
+  it 'returns bad request for an invalid validation' do
+    expect { post '/', {} }.to raise_error 'Validation Failed'
+  end
+end
