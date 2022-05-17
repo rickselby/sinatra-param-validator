a12c711db33821ce220de43455607b0b9410a5ee
Rick Selby <rick@selby-family.co.uk>
2022-05-17 09:37:21 +0100

    Allow symbol definition of parameter types

--- lib/sinatra/param_validator/parameter.rb
+++ lib/sinatra/param_validator/parameter.rb
@@ -11,6 +11,7 @@ module Sinatra
     # Class to validate a single parameter
     class Parameter
       def self.new(value, type, *args)
+        type = type.to_s.capitalize if type.is_a? Symbol
         klass = Object.const_get "Sinatra::ParamValidator::Parameter::#{type}"
         klass.new(value, *args)
       end
