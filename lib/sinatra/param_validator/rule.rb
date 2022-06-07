49726eb36099a9b9ea690f6b5977461e8d4f49a5
Rick Selby <richard.selby@york.ac.uk>
2022-06-07 10:25:05 +0100

    Share camelize method between classes

--- lib/sinatra/param_validator/rule.rb
+++ lib/sinatra/param_validator/rule.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative 'camelize'
 require_relative 'rule/all_or_none_of'
 require_relative 'rule/any_of'
 require_relative 'rule/one_of'
@@ -8,14 +9,14 @@ module Sinatra
   module ParamValidator
     # Class to check a single rule
     class Rule
-      def self.new(name, params, *args, **kwargs)
-        name = camelize(name) if name.is_a? Symbol
-        klass = Object.const_get "Sinatra::ParamValidator::Rule::#{name}"
-        klass.new(params, *args, **kwargs)
-      end
+      class << self
+        include Camelize
 
-      def self.camelize(symbol)
-        symbol.to_s.split('_').map(&:capitalize).join
+        def new(name, params, *args, **kwargs)
+          name = camelize(name) if name.is_a? Symbol
+          klass = Object.const_get "Sinatra::ParamValidator::Rule::#{name}"
+          klass.new(params, *args, **kwargs)
+        end
       end
     end
   end
