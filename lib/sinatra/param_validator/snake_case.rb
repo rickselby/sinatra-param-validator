48710baf44b316b00c5963da2270feca9b435f7e
Rick Selby <richard.selby@york.ac.uk>
2022-06-08 21:42:22 +0100

    Fix cops

--- lib/sinatra/param_validator/snake_case.rb
+++ lib/sinatra/param_validator/snake_case.rb
@@ -5,7 +5,7 @@ module Sinatra
     # Helpers for validating parameters
     module SnakeCase
       def snake_case(string)
-        string.gsub(/([a-z\d])([A-Z])/,'\1_\2').downcase
+        string.gsub(/([a-z\d])([A-Z])/, '\1_\2').downcase
       end
     end
   end
