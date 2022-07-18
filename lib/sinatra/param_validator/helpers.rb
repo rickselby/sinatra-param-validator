7936a0bc779be429fb3ca3e8f46e0096ca5430cf
Rick Selby <richard.selby@york.ac.uk>
2022-07-18 15:38:46 +0100

    Move context functions into helpers
    At this point, if we're extending the application context, these may as well be defined as helpers

--- lib/sinatra/param_validator/helpers.rb
+++ lib/sinatra/param_validator/helpers.rb
@@ -13,12 +13,53 @@ module Sinatra
         raise "Filter params failed: #{e}"
       end
 
+      def param(key, type, default: nil, message: nil, **args, &block)
+        parameter = Parameter.new(params[key], type, **args)
+        _update_params_hash key, parameter, default
+        if parameter.valid?
+          _run_block(key, block) if block
+        else
+          _add_error key, message || parameter.errors
+        end
+      rescue NameError
+        raise 'Invalid parameter type'
+      end
+
+      def rule(name, *args, **kwargs)
+        rule = Rule.new(name, params, *args, **kwargs)
+        unless rule.passes?
+          @_validator_errors[:rules] ||= []
+          @_validator_errors[:rules].push(rule.errors)
+        end
+      rescue NameError
+        raise 'Invalid rule type'
+      end
+
       def validate(klass, identifier)
         identifier = Identifier.new(identifier) if identifier.is_a? Symbol
         definition = settings.validator_definitions.get(identifier.identifier)
         validator = klass.new(&definition)
         validator.run(self, *identifier.args)
         validator.handle_failure(self) unless validator.success?
+      end
+
+      def _add_error(key, error)
+        @_validator_errors[key] = @_validator_errors.fetch(key, []).concat(Array(error))
+      end
+
+      def _run_block(key, block)
+        args = block.arity == 1 ? [self] : []
+        instance_exec(*args, &block)
+      rescue InvalidParameterError => e
+        _add_error key, e.message
+      end
+
+      def _update_params_hash(key, parameter, default)
+        if params.key?(key)
+          params[key] = parameter.coerced unless parameter.coerced.nil?
+        elsif !default.nil?
+          params[key] = default.respond_to?(:call) ? default.call : default
+        end
       end
     end
   end
--- lib/sinatra/param_validator/parser.rb
+++ lib/sinatra/param_validator/parser.rb
@@ -8,50 +8,6 @@ require_relative 'rule'
 
 module Sinatra
   module ParamValidator
-    # Methods to add to the context for handling parameters
-    module ValidatorMethods
-      def _add_error(key, error)
-        @__validator_errors[key] = @__validator_errors.fetch(key, []).concat(Array(error))
-      end
-
-      def param(key, type, default: nil, message: nil, **args, &block)
-        parameter = Parameter.new(params[key], type, **args)
-        _update_params_hash key, parameter, default
-        if parameter.valid?
-          _run_block(key, block) if block
-        else
-          _add_error key, message || parameter.errors
-        end
-      rescue NameError
-        raise 'Invalid parameter type'
-      end
-
-      def rule(name, *args, **kwargs)
-        rule = Rule.new(name, params, *args, **kwargs)
-        unless rule.passes?
-          @__validator_errors[:rules] ||= []
-          @__validator_errors[:rules].push(rule.errors)
-        end
-      rescue NameError
-        raise 'Invalid rule type'
-      end
-
-      def _run_block(key, block)
-        args = block.arity == 1 ? [self] : []
-        instance_exec(*args, &block)
-      rescue InvalidParameterError => e
-        _add_error key, e.message
-      end
-
-      def _update_params_hash(key, parameter, default)
-        if params.key?(key)
-          params[key] = parameter.coerced unless parameter.coerced.nil?
-        elsif !default.nil?
-          params[key] = default.respond_to?(:call) ? default.call : default
-        end
-      end
-    end
-
     # Run the definition in the given scope
     class Parser
       attr_reader :errors
@@ -61,10 +17,9 @@ module Sinatra
       end
 
       def parse(definition, *args)
-        @context.extend(ValidatorMethods)
-        @context.instance_variable_set(:@__validator_errors, {})
+        @context.instance_variable_set(:@_validator_errors, {})
         @context.instance_exec(*args, &definition)
-        @errors = @context.instance_variable_get(:@__validator_errors)
+        @errors = @context.instance_variable_get(:@_validator_errors)
 
         self
       end
