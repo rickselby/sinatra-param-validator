205b1a32e3c2894fa9dd09a04204fa0d8aae2062
Rick Selby <rick@selby-family.co.uk>
2026-09-13 11:58:03 +0100

    Fix new cops

--- spec/sinatra/param_validator_instance_variable_spec.rb
+++ spec/sinatra/param_validator_instance_variable_spec.rb
@@ -3,7 +3,7 @@
 require "sinatra/test_helpers"
 
 # Test that instance variables can be passed around
-# rubocop:disable RSpec/InstanceVariable
+# rubocop:disable-next RSpec/InstanceVariable
 describe Sinatra::ParamValidator do
   include Sinatra::TestHelpers
 
@@ -30,4 +30,3 @@ describe Sinatra::ParamValidator do
     expect(post("/").body).to eq "foo"
   end
 end
-# rubocop:enable RSpec/InstanceVariable
